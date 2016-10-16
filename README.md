ac29a72690e8823e89fdfad5f532328bf69e5814
ssarcandy <ssarcandy@gmail.com>
2016-10-16 22:14:12 +0800

    use argv and add readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # github-clone-all-forks
+
+## Usage
+
+```bash
+$ npm install
+$ mkdir repos_dir
+$ cd repos_dir
+$ node ../index.js <repo_name> 
+```
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var request = require('request');
 var simpleGit = require('simple-git')();
+var repoName = process.argv[2];
+
+if (!repoName) {
+    console.log('[Usage] node index.js <repo>');
+    process.exit(1);
+}
 
 request.get({
-    url: 'https://api.github.com/repos/cglabnccu/3d16hw1/forks',
+    url: 'https://api.github.com/repos/cglabnccu/' + repoName + '/forks',
     headers: {
         'User-Agent': 'request'
     }
 }, function (err, res) {
     var response = JSON.parse(res.body);
-    var forks = response.map(function(repo) {
+    var forks = response.map(function (repo) {
         return {
             name: repo.full_name.replace('/', '_'),
             clone_url: repo.clone_url
@@ -17,7 +23,9 @@ request.get({
 
     // console.log(forks);
 
-    forks.forEach(function(repo) {
-        simpleGit.clone(repo.clone_url, repo.name);
+    forks.forEach(function (repo) {
+        simpleGit.clone(repo.clone_url, repo.name, function () {
+            console.log('cloning ' + repo.name + ' done.');
+        });
     });
 });
